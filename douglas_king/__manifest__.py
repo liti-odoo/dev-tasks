03dc267fe02725a0936e3530d34482752343eebd
Tina <liti@odoo.com>
2025-08-13 14:14:02 -0700

    [ADD] Document Layout Customization

---
+++ douglas_king/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Document Layout Customization",
+    'summary': "Changed the header and footer of the 'Boxed' layout",
+    'version': '1.0',
+    'author': "liti-odoo",
+    'category': 'Invoice',
+    "license": "OPL-1",
+    'data': {
+        #VIEWS
+        "document_customization/models/views/report_templates.xml"
+    }
+
+}
