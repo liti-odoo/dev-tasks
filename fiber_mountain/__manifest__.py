02926bdd7d6a1faeebfef7b40e9d989b75bb6d62
Tina <liti@odoo.com>
2025-08-15 11:42:05 -0700

    [ADD] Price Customization for Custom Attributes

---
+++ fiber_mountain/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Custom Product Variant",
+    'summary': "Calculate part numbers and price based on a set of inputs.",
+    'version': '1.0',
+    'author': "liti-odoo",
+    'category': 'Sale',
+    "license": "OPL-1",
+    'description': """
+    task- FiberMountain : Product Configuration with Variants
+    """,
+    'depends': [
+        "sale"
+    ],
+}
